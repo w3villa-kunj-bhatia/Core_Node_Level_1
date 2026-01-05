d532d0e22da8cf283930d70bcf38df592d71109c
Kunj Bhatia <kunj.bhatia@w3villa.com>
2026-01-05 14:48:55 +0530

    Add file operations and logging functionality to file.js; create hello.txt

--- file.js
+++ file.js
@@ -1,1 +1,42 @@
-const fs = require('fs');
+const fs = require("fs");
+
+const [, , command, fileName, content] = process.argv;
+
+if (!command || !fileName) {
+  console.log("Usage: node file.js <command> <fileName> [content]");
+  console.log("Commands: read, write, delete");
+  process.exit(1);
+}
+
+switch (command) {
+  case "write":
+    fs.writeFile(fileName, content || "", (err) => {
+      if (err) throw err;
+      console.log(`File Created: ${fileName}`);
+    });
+    break;
+
+  case "read":
+    fs.readFile(fileName, "utf-8", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+    break;
+
+  case "append":
+    fs.appendFile(fileName, content || "", (err) => {
+      if (err) throw err;
+      console.log(`Content Appended to: ${fileName}`);
+    });
+    break;
+
+  case "delete":
+    fs.unlink(fileName, (err) => {
+      if (err) throw err;
+      console.log(`File Deleted: ${fileName}`);
+    });
+    break;
+
+  default:
+    console.log("Unknown command. Use read, write, append, or delete.");
+}
